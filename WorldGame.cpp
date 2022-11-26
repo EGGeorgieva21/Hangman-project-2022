058f9ac59ece96938fcb205d5bf663b6f5450bf8
ATSivkov21 <107105722+ATSivkov21@users.noreply.github.com>
2022-11-26 21:16:04 +0200

    Updating code for WorldGame.cpp

--- WorldGame.cpp
+++ WorldGame.cpp
@@ -153,5 +153,156 @@ int main()
 	else 
 	{
 		cout << "Error";
-	}	
+	}
+	/*cout << "Character-";
+	cin >> character;
+	cout << endl;
+	for (int i = 0; i <= 52; i++) {
+		if (char (RandomInt == word[i])){
+			break;
+		}
+		else {
+			RandomInt = 0;
+			break;
+		}
+	}
+	RandomInt = int(RandomInt * RandomInt);
+	while ((RandomInt > 0) && (RandomInt < 10)) {
+		RandomInt = RandomInt / 10;
+	} 
+	//while (RandomInt > 0);
+	if ((RandomInt > 0) && (RandomInt < 1)) {
+		RandomInt = (RandomInt * 10);
+	}
+	else {
+		cout << endl;
+	}
+	while (T != true) {
+		for (int i = 0; i <= 60; i++) {
+			if ((char(RandomInt == word[i])) && (char(RandomInt == alphabet[i]))) {
+				for (int a = 0; a <= 13; a++) {
+					if ((char(RandomInt == easy[a])) && (int(RandomInt >= 0)) && (int(RandomInt <= 3))) {
+						while ((nume <= 3) && (nume >= 0)) {
+							w[a] = easy[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == easy[a])) && (int(RandomInt >= 4)) && (int(RandomInt <= 7))) {
+						while ((nume >= 4) && (nume <= 7)) {
+							w[a] = easy[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == normal[a])) && (int(RandomInt >= 0)) && (int(RandomInt <= 3))) {
+						while ((nume >= 0) && (nume <=3)) {
+							w[a] = normal[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == normal[a])) && (int(RandomInt >= 4)) && (int(RandomInt <= 8))) {
+						while ((nume >= 4) && (nume <= 8)) {
+							w[a] = normal[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == hard[a])) && (int(RandomInt >= 0)) && (int(RandomInt <= 4))) {
+						while ((nume >= 0) && (nume <= 4)) {
+							w[a] = hard[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == hard[a])) && (int(RandomInt >= 5)) && (int(RandomInt <= 10))) {
+						while ((nume >= 5) && (nume <= 10)) {
+							w[a] = hard[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == hard[a])) && (int(RandomInt >= 11)) && (int(RandomInt <= 16))) {
+						while ((nume >= 11) && (nume <= 16)) {
+							w[a] = hard[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == veryHard[a])) && (int(RandomInt >= 0)) && (int(RandomInt <= 5))) {
+						while ((nume >= 0) && (nume <= 5)) {
+							w[a] = veryHard[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == veryHard[a])) && (int(RandomInt >= 6)) && (int(RandomInt <= 12))) {
+						while ((nume >= 6) && (nume <= 12)) {
+							w[a] = veryHard[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == veryHard2[a])) && (int(RandomInt >= 0)) && (int(RandomInt <= 6))) {
+						while ((nume >= 0) && (nume <= 6)) {
+							w[a] = veryHard2[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					if ((char(RandomInt == veryHard2[a])) && (int(RandomInt >= 7)) && (int(RandomInt <= 13))) {
+						while ((nume >= 7) && (nume <= 13)) {
+							w[a] = veryHard2[a];
+							nume = nume + 1;
+						}
+						T = (true);
+						break;
+					}
+					else {
+						cout << "Error" << endl;
+						break;
+					}
+				}
+			}
+			else {
+				RandomInt = char(int(RandomInt + 1));
+				cout << "error character" << endl;
+				break;
+			}
+		}
+	}
+	cin >> character;
+	while (Tru != true) {
+		for (int x = 0; x <= 51; x++) {
+			if (char(character == alphabet[x])) {
+				while (Tr != true) {
+					for (int v = 0; v <= nume; v++) {
+						if (char(character == w[v])) {
+							cout << "correct is -" << character << endl;;
+							break;
+						}
+						else {
+							cout << "is not correct -" << character << endl;
+							break;
+						}
+					}
+				}
+				Tru = (true);
+			}
+			else {
+				Tru = (false);
+			}
+		}
+	}*/
 }
