a44964c6a94ed9b7c5fd0e9930acc0c73d97f610
EGGeorgieva21 <EGGeorgieva21@codingburgas.bg>
2022-12-06 23:37:10 +0200

    Change game into a wordle/hangman. Make a hangman title, make a little human. Put everything into functions and put those in the correct files. They don't work yet, but they will.

--- WorldGame.cpp
+++ WorldGame.cpp
@@ -1,302 +1,33 @@
 #include <iostream>
+#include "hangmanFunctions.cpp"
 #include <iomanip>
-#include <cmath>
-#include <windows.h>
-#include <random>
 #include <string>
 using namespace std;
 // QA - check if we're really using all these libraries once we finish up w/ everything
+
+// global declarations
+string start;
+string word;
+double health = 16;
+string level;  
+bool gameEnded = false;
+int fateChooser;
+
+
 int main()
 {
-	//declarations
-	bool True = (false);
-	int num = 0;
-	int num1 = 0;
-	int num2 = 0;
-	char w[7];
-	string level;
-	default_random_engine eng;
-	int RandomWord = eng();
-	
-	//words:
-	char easy[8]{ 'b', 'a', 'l', 'l', /**/'f', 'o', 'u', 'r' };//3-4
-	string easy1[2]{ "ball", "four" };
-	char normal[9]{ 'l', 'e', 'a', 'f', /**/'b', 'o', 'a', 'r', 'd' };//4-5
-	string normal1[2]{ "leaf", "board" };
-	char hard[17]{ 'f', 'i', 'g', 'h', 't',/**/ 'f', 'l', 'o', 'w', 'e', 'r', /* */'a', 'c', 'c', 'e', 'p', 't' };//5-6
-	string hard1[3]{ "fight", "flower", "accept" };
-	char veryHard[13]{ 'a', 'c', 'c', 'e', 'p', 't',/**/ 'q', 'u', 'i', 'z', 'z', 'e', 'r' };//6-7+
-	string veryHard1[2]{ "accept", "quizzer" };
-	char veryHard2[14]{ 'f', 'i', 'z', 'z', 'i', 'n', 'g', /**/  'w', 'h', 'i', 'z', 'z', 'e', 'r' };//7-8+
-	string veryHard3[2]{ "fizzing", "whizzer" };
+void printingHangman();
+void printingHuman();
+void printingMenuLevel();
+void printingRules();
+void startingTheGame();
+void choosingFate();
+void choosingLevel();
+void automaticLevelAssignment();
+void guessChecker();
 
-	//char word[61]{ 'b', 'a', 'l', 'l', /*4*/'f', 'o', 'u', 'r', /*4/8*/ 'l', 'e', 'a', 'f', /*4/12*/'b', 'o', 'a', 'r', 'd', /*5/17*/ 'f', 'i', 'g', 'h', 't',/*5/22*/ 'f', 'l', 'o', 'w', 'e', 'r', /*6/28*/'a', 'c', 'c', 'e', 'p', 't', /*6/34*/ 'a', 'c', 'c', 'e', 'p', 't',/*6/40*/ 'q', 'u', 'i', 'z', 'z', 'e', 'r', /*7/47*/ 'f', 'i', 'z', 'z', 'i', 'n', 'g',/*7/54*/ 'w', 'h', 'i', 'z', 'z', 'e', 'r' /*7/61*/ };
-	
-//magic. don't touch.
-	cout << setw(7) << "__" << endl;
-	cout << setw(5) << "|" << setw(3) << "|" << endl;
-	cout << setw(5) << "|" << setw(4) << "\\O/" << endl;
-	cout << setw(5) << "|" << setw(3) << "|" << endl;
-	cout << setw(5) << "|" << setw(2) << "/" << setw(2) << "\\" << endl;
-	cout << setw(10) << "|_____" << endl;
-
-	cout << setw(48) << "_________________________________" << endl;
-	cout << setw(16) << "|" << setw(32) << "|" << endl;
-	cout << setw(16) << "|" << setw(19) << "MENU GAME" << setw(13) << "|" << endl;
-	cout << setw(16) << "|" << setw(32) << "|" << endl;
-	cout << setw(16) << "|" << setw(19) << "level - 1 (Easy)" << setw(13) << "|" << endl;
-	cout << setw(16) << "|" << setw(21) << "level - 2 (Normal)" << setw(11) << "|" << endl;
-	cout << setw(16) << "|" << setw(19) << "level - 3 (Hard)" << setw(13) << "|" << endl;
-	cout << setw(16) << "|" << setw(24) << "level - 4 (Very Hard)" << setw(8) << "|" << endl;
-	cout << setw(16) << "|" << setw(28) << "level - 5 (Very Hard - 1)" << setw(4) << "|" << endl;
-	cout << setw(48) << "|_______________________________|" << endl;
-
-	// cout << "________________________________________________" << endl;
-	//for (int i = 1; i <= 24; i++)
-	//{
-	//	cout << "|                                               |" << endl;
-	//	// print box parameters for main game
-	//}
-	//cout << "_________________________________________________" << endl; 
-
-//rules
-	cout << "Rules: When asked start, echo start. otherwise, death." << endl;
-	cout << "You want to end the game... perhaps a 0 would do... ? " << endl;
-	cout << "So, start?\n" << endl;
-	string start;
-	cin >> start;
-
-
-//Don't touch, this part officially WORKS. DONT. TOUCH. I WILL FIND YOU!!!!!!!!!!!!!!!!!!!!!!!!!!
-	while ((start != "Start") || (start != "start") || (start != "START")){
-		if ((start == "Start") || (start == "start") || (start == "START")){
-			//start the game
-			cout << "welcome" << endl;
-			break;
-		}
-		else if(start == "0"){
-			cout << "Bye." << endl;
-			exit(0); //stops the game 
-		}
-		else{
-			//restart the code
-			cout << "Not quite. Are you sure you spelled it correctly? Or perhaps you meant to say 0?" << endl;
-			cin >> start;
-		}
-	}
-	
-
-	cout << "Now, choose your level. You may choose 1 through 4. Which level is how hard, though, even we don't know." << endl;
-	cout << "level - ";
-	cin >> level;
-
-
-	//here the magic ends.
-
-
-	cout << endl;
-	// level - 1,2,3,4
-	RandomWord = (RandomWord * RandomWord); 
-	while ((RandomWord <= 10) && (RandomWord >= 0)) 
-	{
-		RandomWord = RandomWord / 10;
-	}	
-	
-	// random_int < 10 and random_int > 0 
-	if ((level == "easy") || (level == "1")) 
-	{
-		if (string(easy1[RandomWord]) == "ball")
-		{
-			for (int i = 0; i <= 3; i++) 
-			{
-				w[i] = easy[i];
-				num1 = 4;
-			}
-		}
-		else {
-			for (int i = 4; i <= 7; i++) 
-			{
-				w[num] = easy[i];
-				num = num + 1;
-				num1 = 8;
-			}
-		}
-	}
-	// easy: word ("ball" and "four")
-	// w[i]- is list for characters on words 
-	if ((level == "normal") || (level == "2")) 
-	{
-		if (string(normal1[RandomWord]) == "leaf")
-		{
-			for (int i = 0; i <= 3; i++) 
-			{
-				w[i] = easy[i];
-				num1 = 4;
-			}
-		}
-		else {
-			for (int v = 4; v <= 8; v++) 
-			{
-				w[int(num)] = easy[v];
-				num = num + 1;
-				num1 = 5;
-			}
-		}
-	}
-	if ((level == "hard") || (level == "3")) 
-	{
-		if (string(hard1[int(RandomWord)]) == "fight")
-		{
-			for (int i = 0; i <= 4; i++) 
-			{
-				w[i] = easy[i];
-				num1 = 5;
-			}
-		}
-		if (string(hard1[int(RandomWord)]) == "flower")
-		{
-			for (int v = 5; v <= 10; v++) 
-			{
-				w[int(num)] = easy[int(v)];
-				num = num + 1;
-				num1 = 6;
-			}
-		}
-		else 
-		{
-			for (int c = 11; c <= 16; c++) 
-			{
-				w[num] = easy[int(c)];
-				num = num + 1;
-				num1 = 6;
-			}
-		}
-	}
-	if ((level == "Very Hard") || (level == "4")) 
-	{
-		if (string(veryHard1[int(RandomWord)]) == "accept")
-		{
-			for (int i = 0; i <= 5; i++) 
-			{
-				w[i] = easy[i];
-				num1 = 6;
-			}
-		}
-		else {
-			for (int v = 6; v <= 12; v++) 
-			{
-				w[num] = easy[int(v)];
-				num = num + 1;
-				num1 = 7;
-			}
-		}
-	}
-	if ((level == "Very Hard - 1") || (level == "5"))
-	{
-		if (string(veryHard3[int(RandomWord)]) == "fizzing")
-		{
-			for (int i = 0; i <= 6; i++) 
-			{
-				w[i] = easy[i];
-				num1 = 7;
-			}
-		}
-		else 
-		{
-			for (int v = 7; v <= 13; v++) 
-			{
-				w[int(num)] = easy[int(v)];
-				num = num + 1;
-				num1 = 7;
-			}
-		}
-	}	
-
-
-cout << level << endl;
-
-/*
-int index;
-int counter = 0;
-while (counter < 1)
-{
-	string character;
-	cout << "Character-";
-	cin >> character;
-	cout << endl;
-	for (int v = 0; v <= 6; v++)
-	{
-		if (character == w[v])
-		{
-			cin << index;
-
-			cout << "True-" << character << endl;
-			break;
-		}
-
-		// True - a
-		else
-		{
-			cout << "False-" << character << endl;
-			break;
-		}
-		// False - a
-
-	}
-
-	counter++;
-} */
-/*char character;
-cout << "What's your guess?" << endl;
-cin >> character;*/
-
-
-//char character;
-	cout << "character -";
-	char character;
-	cout << endl;
-	
-	// hp percentage here, let's just set it to 6 for texting
-	for (int v=0; v<=6; v++)
-	{
-	
-		cout << "What's your guess?" << endl;
-		cin >> character;
-		if (character == w[v]){
-			cout << character << "is in word" << endl;
-			//cout the place of the char in the word, somehow visually? have an idea 4 this
-		}
-		else{
-			cout << character << "isn't in the word" << endl;
-			// make a vector which adds the character to it, later make an output of that vector as tried letters not in word
-		}
-
-
-		/*while(character) <= 7){
-			if (character == w[v]) 
-			{				
-				cout << "True-" << character << endl;
-				break;
-			}
-			// True - a
-			else 
-			{
-				cout << "False-" << character << endl;
-				break;
-			}
-			// False - a
-		}
-		*/
-	}
-
-
-
-
-
-
-
-
-
-
+cout << "youve entered int main";
 
 }   
+
+
--- hangmanFunctions.cpp
+++ hangmanFunctions.cpp
@@ -1,7 +1,281 @@
-#include<iostream>
-#include<vector>
-#include"hangmanFunctions.h"
-
+#include <iostream>
+#include "WorldGame.cpp"
+#include <iomanip>
+#include <string>
 using namespace std;
 //Add any extra libraries as needed.
 // Feel free to define your functions here. Add comments to say what they do, if necessary.
+
+
+
+void printingHangman() //prints the game title in big letters
+{
+     cout << setw(10) << " _                  _" << endl;
+     cout << setw(10) << "| |                | |" << endl;
+     cout << setw(10) << "| |                | |" << endl;
+     cout << setw(10) << "| |                | |" << endl;
+     cout << setw(10) << "| |                | |     ________.   _    _________     ___________  _    _   _  _   _  _      ________.   _    _________" << endl;
+     cout << setw(10) << "| |                | |    / _____  |  | |  / ______  |   / /       \\ \\| |  | | / /| | / /| |    / _____  |  | |  / ______  |" << endl;
+     cout << setw(10) << "| |----------------| |   / /     | |  | | / /      | |  / /           | |  | |/ / | |/ / | |   / /     | |  | | / /      | |" << endl;
+     cout << setw(10) << "| |----------------| |  /_/      | |  | |/ /       | |  | |           | |  | | /  | | /  | |  /_/      | |  | |/ /       | |" << endl;
+     cout << setw(10) << "| |                | |           | |  | | /        | |  \\ \\           | |  | |/   | |/   | |           | |  | | /        | |" << endl;
+     cout << setw(10) << "| |                | |    _______| |  | |/         | |   \\_\\_______/_/| |  | |    | |    | |    _______| |  | |/         | |" << endl;
+     cout << setw(10) << "| |                | |   / /     | |  | |          | |   _            | |  | |    | |    | |   / /     | |  | |          | |" << endl;
+     cout << setw(10) << "| |                | |  | |      | |  | |          | |  | |           / /  | |    | |    | |  | |      | |  | |          | |" << endl;
+     cout << setw(10) << "|_|                |_|   \\_\\_____|_|  |_|          |_|  \\ \\__________/ /   |_|    |_|    |_|   \\_\\_____|_|  |_|          |_|" << endl;
+     cout << setw(10) << "                                                         \\____________/" << endl;
+}
+
+void printingHuman() //Prints the little human
+{
+    cout << setw(7) << "__" << endl;
+	cout << setw(5) << "|" << setw(3) << "|" << endl;
+	cout << setw(5) << "|" << setw(4) << "\\O/" << endl;
+	cout << setw(5) << "|" << setw(3) << "|" << endl;
+	cout << setw(5) << "|" << setw(2) << "/" << setw(2) << "\\" << endl;
+	cout << setw(10) << "|_____" << endl;
+}
+
+
+void printingMenuLevel() //Prints the level menu 
+{
+    cout << setw(48) << "_________________________________" << endl;
+	cout << setw(16) << "|" << setw(32) << "|" << endl;
+	cout << setw(16) << "|" << setw(19) << "MENU GAME" << setw(13) << "|" << endl;
+	cout << setw(16) << "|" << setw(32) << "|" << endl;
+	cout << setw(16) << "|" << setw(19) << "level - 1 (Easy)" << setw(13) << "|" << endl;
+	cout << setw(16) << "|" << setw(21) << "level - 2 (Normal)" << setw(11) << "|" << endl;
+	cout << setw(16) << "|" << setw(19) << "level - 3 (Hard)" << setw(13) << "|" << endl;
+	cout << setw(16) << "|" << setw(24) << "level - 4 (Very Hard)" << setw(8) << "|" << endl;
+	cout << setw(16) << "|" << setw(28) << "level - 5 (Very Hard - 1)" << setw(4) << "|" << endl;
+	cout << setw(48) << "|_______________________________|" << endl;
+}
+
+void printingRules() //Prints the rules
+{ 
+    cout << "Rules: When asked start, echo start. otherwise, death." << endl;
+	cout << "If you want to end the game... perhaps a 0 would do... ? " << endl;
+	cout << "So, start?\n" << endl;
+	cin >> start;
+}
+
+void startingTheGame() //The part of the code when you type "start" to start the game
+{
+    while ((start != "Start") || (start != "start") || (start != "START")){
+		if ((start == "Start") || (start == "start") || (start == "START")){
+			//start the game
+			cout << "welcome" << endl;
+			break;
+		}
+		else if(start == "0"){
+			cout << "Bye." << endl;
+			exit(0); //stops the game 
+		}
+		else{
+			//restart the code
+			cout << "Not quite. Are you sure you spelled it correctly? Or perhaps you meant to say 0?" << endl;
+			cin >> start;
+		}
+	}
+}
+
+void choosingLevel() //Choosing your level
+{
+    cout << "Enter your level (1 ~ 5): ";
+    cin >> level;
+    cout << endl;
+
+    
+}
+
+void choosingFate()
+{
+    cout << "Choose one your fate. 1 or 2? - ";
+    cin >> fateChooser;
+    cout << endl;
+}
+
+void automaticLevelAssignment()
+{
+    if (level == "easy" || level == "1") //level check;
+    {
+        if(fateChooser == 1)
+        {
+            cout << "Your hint is: round, smooth. Monkeys all around the world worship it. They run around and kick it... It even has its own sport dedicated to it." << endl;
+            word = "ball";
+        }    
+        else
+        {
+            cout << "Your hint is: A very, very small human. Some are against its death while it is still in primal form. Those guys are weirdoes, do not listen to them." << endl;
+            word = "baby";
+        }
+    }
+    else if (level == "normal" || level == "2")
+    {
+        if(fateChooser == 1)
+        {
+            cout << "Your hint is: An item often used in schools and other professional fascilities, with the intention to torture its readers." << endl;
+            cout << "A black marker is used to add information to it. It usually hangs on a wall, where its knowledge can be displayed to all who dare look.";
+            word =  "board";
+        }    
+        else
+        {
+            cout <<"Your hint is: This is a measurement for bread. Good luck. And no, the word is not piece.";
+            word = "slice";
+        }
+    }
+    else if (level == "hard" || level == "3")
+    {
+        if(fateChooser == 1)
+        {
+            cout << "What button always comes before the decline button on a pop-up asking for some sort of permission?";
+            word = "accept";
+        }    
+        else
+        {
+            cout<< "A fancy word for cloth.";
+            word = "fabric"; 
+        }
+    }
+    else if (level == "veryHard" || level == "4")
+    {
+        if(fateChooser == 1)
+        {
+            cout << "Long eared angels. Fluffy as a cloud, these little cottonballs love jumping, carrots, and are a great house pet. Write the word in plural.";
+            word = "rabbits";
+        }    
+        else
+        {
+            cout << "A tool you use for cleaning, with a purpose of sucking up dust.";
+            word = "vacuum";
+        }
+    }
+    else if (level == "veryHard1" || level == "5")
+    {
+        if(fateChooser == 1)
+        {
+            cout << "A vegetable, having thick green or purple leaves surrounding a spherical heart or head of leaves.";
+            word = "cabbage";
+        }    
+        else
+        {
+            cout << "Addicts who spends their time in casinos, betting money and playiing games, trying to win more money. More often or not, they rob themselves and end up on the streets. Word is in singular.";
+            word = "gambler";
+        }
+    }
+}
+
+void guessChecker() //Checks if your word is the needed word
+{
+    while(health > 0 || gameEnded != false)
+    { 
+        cout << "Enter what you think the word is: ";
+        string guess;
+        cin >> guess;
+        cout << endl;
+        
+        
+            if(guess == word)
+            {
+                cout << "Grats, ya won" << endl;
+                gameEnded = true;
+
+                cout << "You guessed: " << guess << endl; 
+                cout << "And the word was: " << word << endl;
+                break;
+            }
+            
+		    else //losing hp part 
+		    {
+                if (level == "easy" || level == "1")
+                {
+                    health -= 1;
+                    cout << guess << " is not a the correct word." << endl;
+                    cout << endl;
+			        if(health <= 0)//dying part
+                    {
+                        health = 0;
+                        cout << "You have 0 health left. You've lost." << endl;
+                        gameEnded = true;
+                        break;
+                    }
+                    else
+                    {
+			            cout << "You have " << health <<  " health left. Feel threatened." << endl; 
+                    }
+                }
+                else if (level == "normal" || level == "2")
+                {
+                    health -= 2;
+                    cout << guess << " is not the correct word." << endl;
+                    cout << endl;
+			        if(health <= 0)//dying part
+                    {
+                        health = 0;
+                        cout << "You have 0 health left. You've lost." << endl;
+                        gameEnded = true;
+                        break;
+                    }
+                    else
+                    {
+			            cout << "You have " << health <<  " health left. Feel threatened." << endl; 
+                    }
+                    
+                }
+                else if (level == "hard" || level == "3")
+                {
+                    health -= 3;
+                    cout << guess << " is not the correct word." << endl;
+                    cout << endl;
+			        if(health <= 0)//dying part
+                    {
+                        health = 0;
+                        cout << "You have 0 health left. You've lost." << endl;
+                        gameEnded = true;
+                        break;
+                    }
+                    else
+                    {
+			            cout << "You have " << health <<  " health left. Feel threatened." << endl; 
+                    }
+                   
+                }
+                else if (level == "veryHard" || level == "4")
+                {
+                    health -= 3.5;
+                    cout << guess << " is not the correct word." << endl;
+                    cout << endl;
+                    if(health <= 0)//dying part
+                    {
+                        health = 0;
+                        cout << "You have 0 health left. You've lost." << endl;
+                        gameEnded = true;
+                        break;
+                    }
+                    else
+                    {
+			            cout << "You have " << health <<  " health left. Feel threatened." << endl; 
+                    }
+                    
+                }
+                else if (level == "veryHard1" || level == "5")
+                {
+                    health -= 4;
+                    cout << guess << " is not the correct word." << endl;
+                    cout << endl;
+                    if(health <= 0)//dying part
+                    {
+                        health = 0;
+                        cout << "You have 0 health left. You've lost." << endl;
+                        gameEnded = true;
+                        break;
+                    }
+                    else
+                    {
+			            cout << "You have " << health <<  " health left. Feel threatened." << endl; 
+                    }
+                    
+                }
+		    }
+    }
+}
